db9aaf21b197216ff37ced34332f2dd3862f4fe6
Egor Frolov <noame.acc@gmail.com>
2026-09-21 15:02:25 +0300

    Добавлен include string и сообщение о завершении

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 
 int main() {
     // Выводим приветственное сообщение пользователю
     std::string msg = "Hello";
     std::cout << msg << std::endl;
+    std::cout << "Программа завершена успешно" << std::endl;
 }
