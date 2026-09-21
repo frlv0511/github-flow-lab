7343cae72b59da37400c6d097922c5b8bfa0ba13
Egor Frolov <noame.acc@gmail.com>
2026-09-21 15:10:37 +0300

    Переименована переменная, вынесен повторяющийся код в функцию printMessage

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 
+void printMessage(const std::string& greeting) {
+    std::cout << greeting << std::endl;
+}
+
 int main() {
     // Выводим приветственное сообщение пользователю
-    std::string msg = "Hello";
-    std::cout << msg << std::endl;
-    std::cout << "Программа завершена успешно" << std::endl;
+    std::string greeting = "Hello";
+    printMessage(greeting);
+    printMessage("Программа завершена успешно");
     return 0;
 }
